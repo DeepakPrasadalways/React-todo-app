e2680af7cf1e5697fd34b48bd70d1b45684eb384
dp <deepakraja852456@gmail.com>
2021-03-15 19:41:53 +0530

    Updates

---
+++ precache-manifest.739f5f5c03dd3ec11258ba648a6f1a8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be971cbeb44511dd6aa3f217255204fd",
+    "url": "/React-todo-app/index.html"
+  },
+  {
+    "revision": "a470c72b84d08ab0d82c",
+    "url": "/React-todo-app/static/css/main.c6e9b1ea.chunk.css"
+  },
+  {
+    "revision": "01ac84dda022c12bf2ef",
+    "url": "/React-todo-app/static/js/2.d639c448.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-todo-app/static/js/2.d639c448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a470c72b84d08ab0d82c",
+    "url": "/React-todo-app/static/js/main.a7384c0f.chunk.js"
+  },
+  {
+    "revision": "e78960798ba553b8dc81",
+    "url": "/React-todo-app/static/js/runtime-main.8b6f6341.js"
+  }
+]);
